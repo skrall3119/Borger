ec08f9776d1e7612a17d0db16db155b8fa1a4b0b
skrall3119 <alexjanci@yahoo.com>
2020-04-13 15:30:22 -0500

    Initial Commit

---
+++ Borger/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Borger
+//
+//  Created by Alex Janci on 4/13/20.
+//  Copyright © 2020 Alex Janci. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
